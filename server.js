31055ee912f347377208290e888ef98519d82932
Scott Currie <vitriolik@gmail.com>
2018-10-24 10:29:00 -0700

    Connected API's tp replace local data.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 require('dotenv').config();
 
@@ -14,27 +15,51 @@ app.use(cors());
 app.listen(PORT, () => console.log(`App is up on $ PORT`));
 
 app.get('/location', (request, response, next) => {
-  try {
-    const locationData = searchToLatLong(request.query.data);
-    response.send(locationData);
-  }
-  catch (exception) {
-    console.log(new Error());
-  }
+  getLocation(request.query.data)
+    .then(locationData => response.send(locationData))
+    .catch(error => handleError(error, response));
+
 });
 
-app.get('/weather', (request, response)=>{
-  const locationData = searchToLatLong(request.query.data);
-  const weatherData = searchWeather(locationData);
-  response.send(weatherData);
-})
-
-function searchToLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
-  location.search_query = query;
-  return location;
+function getLocation(query) {
+  console.log('This is happeing.');
+  const _URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(_URL)
+    .then(data => {
+      if (! data.body.results.length) {throw 'No data'}
+      else {
+        let location = new Location(data.body.results[0]);
+        location.search_query = query;
+        console.log(location);
+        return location;
+      }
+    });
 }
+
+function handleError(err, res) {
+  console.error('ERR', err);
+  if (res) res.status(500).send('Sorry, something went wrong.');
+}
+
+
+app.get('/weather', getWeather);
+
+function getWeather(request, response) {
+  const _URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  return superagent.get(_URL)
+    .then(result => {
+      const weatherSummaries = [];
+      result.body.daily.data.forEach(day => {
+        // console.log('day', day);
+        const summary = new Weather(day);
+        // console.log('summary', summary);
+        weatherSummaries.push(summary);
+      });
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error ,response));
+}
+
 
 function Location(data) {
   this.formatted_query = data.formatted_address;
@@ -42,46 +67,8 @@ function Location(data) {
   this.longitude = data.geometry.location.lng;
 }
 
-/////weather
 
-function searchWeather(location){
-  const darkData = require('./data/darksky.json');
-  const dailyWeatherArr = [];
-
-  darkData.daily.data.forEach(daysData => {
-    const weather = new Weather(daysData.summary, daysData.time);
-    dailyWeatherArr.push(weather);
-  })
-
-  return dailyWeatherArr;
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
-
-function convertUnixTime(unixTime) {
-  let monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-  let dayOfWeekArr = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-  let date = new Date(unixTime*1000);
-  let month = monthName[date.getMonth()];
-  let year = date.getFullYear();
-  let dayOfMonth = date.getDate();
-  let dayOfWeek = dayOfWeekArr[date.getDay()];
-
-  return dayOfWeek + ' ' + month + ' ' + dayOfMonth + ' ' + year;
-}
-
-function Weather(summary, time) {
-  this.forecast = summary;
-  this.time = convertUnixTime(time);
-}
-
-//////////errors
-function Error() {
-  this.status = 500;
-  this.respoonseText = 'Sorry, something went wrong';
-}
-
-
-// {
-//   status: 500,
-//   responseText: "Sorry, something went wrong",
-//   ...
-// }
